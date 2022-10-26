ad52203aef17d9490a2cfc5de8753e2938cd6a04
Jérôme Lesaux <jerome.lesaux.ext@ovhcloud.com>
2022-10-26 09:16:32 +0200

    add: add package name test.

--- z80Format_test.go
+++ z80Format_test.go
@@ -1,8 +1,9 @@
-package z80format
+package z80format_test
 
 import (
 	"testing"
 
+	"github.com/jeromelesaux/z80format"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,7 +32,7 @@ func TestFormat(t *testing.T) {
 			 jp asicoff
 `
 	expected := "FontOk\n;brk\n\tLD de,my_font ; SpriteHardPtr\n\tLD l,a\n\tLD h,0\n\tADD hl,hl ;*2\n\tADD hl,hl ;*4\n\tADD hl,hl ;*8\n\tADD hl,hl ;*16\n\tADD hl,hl ;*32\n\tADD hl,hl ;*64\n\tADD hl,hl ;*128\n\tADD hl,hl ;*256 octets taille d'une sprite hard\n\tADD hl,de ; hl pointe sur la bonne lettre dans la fonte\n\n\tLD A,I ; numero du sprite\n\tADD A,#40 ; sprites sont en #4000 dans l'asic\n\tLD D,A ; adresse du sprite\n\tLD E,0\n\tLD bc,#00FF+1\nldir\n\tJP asicoff\n"
-	res, err := Format(code)
+	res, err := z80format.Format(code)
 	assert.NoError(t, err)
 	assert.Equal(t, expected, res)
 }
